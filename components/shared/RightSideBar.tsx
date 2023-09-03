fc1ba3742765674576a37f6228ce26cc7d13d6d3
Rajat Ghosh <rajatghosh@rajats-mbp.speedport.ip>
2023-09-03 14:24:47 +0200

    Added the layout for bottom and right side bar.

--- components/shared/RightSideBar.tsx
+++ components/shared/RightSideBar.tsx
@@ -1,5 +1,16 @@
 function RightSideBar() {
-  return <h1>Right bar</h1>;
+  return (
+    <section className="custom-scrollbar rightsidebar">
+      <div className="flex flex-1 flex-col justify-start">
+        <h3 className="text-heading4-medium text-light-1">
+          Suggested communities
+        </h3>
+      </div>
+      <div className="flex flex-1 flex-col justify-start">
+        <h3 className="text-heading4-medium text-light-1">Suggested users</h3>
+      </div>
+    </section>
+  );
 }
 
 export default RightSideBar;
